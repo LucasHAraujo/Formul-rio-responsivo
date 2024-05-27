904c2d4f12e12b96d7dbc51d56cb54293a379c4f
LucasHAraujo <lucas.araujo33712@gmail.com>
2024-05-27 15:18:00 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('genero').addEventListener('change', function() {
-    var caixaTexto = document.getElementById('caixaTexto');
-    if (this.value === 'outros') {
-      caixaTexto.style.display = 'block';
-    } else {
-      caixaTexto.style.display = 'none';
-    }
-  });
